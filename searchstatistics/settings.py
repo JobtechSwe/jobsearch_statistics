af3f541841a3fa4236ac51f9f325c9f55e4faa67
Erik Melander <erik.melander@jobtechdev.se>
2019-11-18 13:29:35 +0100

    Fixed default alias

--- searchstatistics/settings.py
+++ searchstatistics/settings.py
@@ -5,4 +5,4 @@ ES_PORT = os.getenv("ES_PORT", 9200)
 ES_USER = os.getenv("ES_USER")
 ES_PWD = os.getenv("ES_PWD")
 
-ES_INDEX = os.getenv("ES_INDEX", "search-event-read")
+ES_INDEX = os.getenv("ES_INDEX", "search-events")
